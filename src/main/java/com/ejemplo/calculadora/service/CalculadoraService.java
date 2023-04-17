b9fd586318e57ecbd0dde951b1cc77a22773c9bc
DESKTOP-6VHGKA6\Karolinho_HP <kjcr1985@gmail.com>
2023-04-18 00:20:59 +0200

    implementacion de control de errores en la clase service

--- src/main/java/com/ejemplo/calculadora/service/CalculadoraService.java
+++ src/main/java/com/ejemplo/calculadora/service/CalculadoraService.java
@@ -13,16 +13,28 @@ public class CalculadoraService {
 	public String calculadora(Double valor1, Double valor2, String operacion) {
 		String result = "";
 			
-		if(operacion == "suma") {
-			Suma s1 = new Suma(valor1, valor2);
-			result = s1.toString();
+		try {
+			if(operacion == "suma") {
+				Suma s1 = new Suma(valor1, valor2);
+				result = s1.toString();
+			}
+			else if(operacion == "resta"){
+				Resta r1 = new Resta(valor1, valor2);
+				result = r1.toString();
+			}
+			else {
+				new Throwable("De momento no existe esa operacion");
+				return null;
+			}
+			
+			return result;
+			
 		}
-		else if(operacion == "resta"){
-			Resta r1 = new Resta(valor1, valor2);
-			result = r1.toString();
+		catch (Exception e) {
+			new Exception(e.getMessage());
+			return null;
 		}
 		
-		return result;
 	}
 	
 }
